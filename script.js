d935cb4f05078b4e0bfc3197c11e678806c5b5e3
Bruno-Gdos <Brunocosta1919@hotmail.com>
2022-09-10 21:53:35 -0300

    Update 10/09

--- script.js
+++ script.js
@@ -64,12 +64,19 @@ function Apresentar(){
 
 
   document.getElementById('resultado').innerHTML = `Olá me chamo ${resultado[0]}, tenho ${resultado[1]} anos, escolhi no formulário a data ${resultado[2]} e sou do sexo ${resultado[3]}.
+
 O número de caracteres colocados na apresentação foi: ${resultado[4].length}
-E o texto foi: ${resultado[4]}`;
+E o texto foi: ${resultado[4]}
+
+Gostaria de acrescentar também que, Bruno é um cara muito legal, e que eu recomendo ele para qualquer trabalho, ele merece essa vaga.`;
 
 document.getElementById('resultado2').innerHTML = `Olá me chamo ${resultado[0]}, tenho ${resultado[1]} anos, escolhi no formulário a data ${resultado[2]} e sou do sexo ${resultado[3]}.
+
 O número de caracteres colocados na apresentação foi: ${resultado[4].length}
-E o texto foi: ${resultado[4]}`;
+E o texto foi: ${resultado[4]}
+
+Gostaria de acrescentar também que, Bruno é um cara muito legal, e que eu recomendo ele para qualquer trabalho, ele merece essa vaga.`;
+;
 }
 
 function Copia(){
@@ -87,6 +94,7 @@ function Limpa(){
   document.getElementById("texto").value = '';
   document.getElementById("nome").value = '';
   document.getElementById("idade").value = '';
+  document.getElementById('sexo').checked = false;
   document.getElementById("data").value = '';
   document.getElementById('painel').checked = false;
   document.getElementById('segundo-painel').style.display = 'none';
