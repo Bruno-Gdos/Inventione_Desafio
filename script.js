8d42bfba735094fba4dc22ae2ab58103726e7691
Bruno-Gdos <Brunocosta1919@hotmail.com>
2022-09-11 12:54:34 -0300

    Form_to_Sheet Update

--- script.js
+++ script.js
@@ -31,39 +31,45 @@ function formataData(data){
   return `${dia}/${mes}/${ano}`;
 }
 
-function Apresentar(){
-  var resultado = [];
-  var sexo = document.getElementsByName('sexo');
-  console.log(sexo.length);
+const Forms = () => {
 
-  for(i = 0; i < sexo.length; i++) {
-    if(sexo[i].checked){
-    var sexo = sexo[i];
-    console.log(sexo);
+    var resultado = [];
+    var sexo = document.getElementsByName('sexo');
+  
+    for(i = 0; i < sexo.length; i++) {
+      if(sexo[i].checked){
+      var sexo = sexo[i];
+      }
+  }
+  
+    var input = [document.querySelector("#nome"), document.querySelector("#idade"), document.querySelector("#data"),sexo, document.getElementById("texto")];
+  
+    for (let indice=0; indice < input.length; indice++){
+      
+      if (input[indice].id == "data"){
+        var data = formataData(input[indice].value);
+        resultado.push(data);
+      }else{
+        if (input[indice].value == ""){
+          alert("Preencha todos os campos");
+          return;
+        }
+        var texto = input[indice].value;
+        resultado.push(texto);
+      }
     }
+    return resultado;
 }
 
-  var input = [document.querySelector("#nome"), document.querySelector("#idade"), document.querySelector("#data"),sexo, document.getElementById("texto")];
+function Apresentar(){
 
-  for (let indice=0; indice < input.length; indice++){
-    
-    if (input[indice].id == "data"){
-      var data = formataData(input[indice].value);
-      resultado.push(data);
-    }else{
-      if (input[indice].value == ""){
-        alert("Preencha todos os campos");
-        return;
-      }
-      var texto = input[indice].value;
-      resultado.push(texto);
-    }
-  }
-
-  document.getElementById('apresentacao').style.display = 'block';
+  var resultado = Forms();
 
 
-  document.getElementById('resultado').innerHTML = `Olá me chamo ${resultado[0]}, tenho ${resultado[1]} anos, escolhi no formulário a data ${resultado[2]} e sou do sexo ${resultado[3]}.
+document.getElementById('apresentacao').style.display = 'block';
+
+
+document.getElementById('resultado').innerHTML = `Olá me chamo ${resultado[0]}, tenho ${resultado[1]} anos, escolhi no formulário a data ${resultado[2]} e sou do sexo ${resultado[3]}.
 
 O número de caracteres colocados na apresentação foi: ${resultado[4].length}
 E o texto foi: ${resultado[4]}
@@ -89,17 +95,25 @@ function Copia(){
 
 function Limpa(){
   document.getElementById('apresentacao').style.display = 'none';
-  document.getElementById('resultado').innerHTML = '';
-  document.getElementById('resultado2').innerHTML = '';
-  document.getElementById("texto").value = '';
-  document.getElementById("nome").value = '';
-  document.getElementById("idade").value = '';
-  document.getElementById('sexo').checked = false;
-  document.getElementById("data").value = '';
   document.getElementById('painel').checked = false;
   document.getElementById('segundo-painel').style.display = 'none';
   alert("Formulário limpo, obrigado por usar o formulário");
 }
 
-  
+const handleSubmit = (event) => {
+  resultado = Forms();
+  event.preventDefault();
+  alert('Formulário enviado com sucesso');
+
+  fetch('https://api.sheetmonkey.io/form/jKo1g4cjViWVKkkVpVdriK',{
+    method: 'post',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body:JSON.stringify({Nome: resultado[0], Idade: resultado[1], Data_Escolhida: resultado[2], Sexo: resultado[3], Texto: resultado[4]})
+});
+}
+
+document.querySelector('form').addEventListener('submit', handleSubmit); 
 
